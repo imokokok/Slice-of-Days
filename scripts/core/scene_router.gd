9651a408a702241633e476b1cea75607522a478b
imokokok <imokokok123@gmail.com>
2026-09-08 21:15:01 +0800

    Revert "Add protected NPC survey results view"
    This reverts commit ad33f72f5bd5803a09bf020ee1598d46bc06073a.

--- scripts/core/scene_router.gd
+++ scripts/core/scene_router.gd
@@ -3,7 +3,6 @@ extends Node
 const MAIN_MENU := "res://scenes/main_menu.tscn"
 const TOWN_DAY := "res://scenes/town_day.tscn"
 const TAROT_TABLE := "res://scenes/tarot_table.tscn"
-const NPC_SURVEY_RESULTS := "res://scenes/npc_survey_results.tscn"
 
 
 func go_to(path: String) -> void:
@@ -24,6 +23,3 @@ func town_day() -> void:
 func tarot_table() -> void:
 	go_to(TAROT_TABLE)
 
-
-func npc_survey_results() -> void:
-	go_to(NPC_SURVEY_RESULTS)
